30260e29ae55683147afe26bdc3bfcb916477d1d
Evan Gipson <evangipson@gmail.com>
2017-04-15 11:19:19 -0500

    Add ability to generate dynamic random number
    Oh look at how vue-y stuff is! neat! Reactivity kind of sucks but I'm
    getting used to where to put things in the export default object.

--- src/components/random-number.vue
+++ src/components/random-number.vue
@@ -1,16 +1,19 @@
 <template>
-  <section class="hero is-light">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Random Number Generator
-        </h1>
-        <h2 class="subtitle">
-            Your random number is... <em><strong>{{randomNumber}}</strong></em>!
-        </h2>
-      </div>
-    </div>
-  </section>
+    <section class="columns">
+        <div class="column is-three-quarters">
+            <h1 class="title">
+                Random Number Generator
+            </h1>
+            <h2 class="subtitle">
+                Your random number is... <em><strong v-cloak>{{randomNumber}}</strong></em>!
+            </h2>
+        </div>
+        <div class="column">
+            <button class="button content is-info" v-on:click="success">
+                Give me another number!
+            </button>
+        </div>
+    </section>
 </template>
 
 <!-- Set up our export -->
@@ -20,10 +23,27 @@ function generateLargeRandomNumber() {
     return utils.random(-1383574.1,13135030);
 };
 export default {
-  data () {
-    return {
-      randomNumber: generateLargeRandomNumber()
+    methods: {
+        success() {
+            this.randomNumber = generateLargeRandomNumber();
+            this.$toast.open({
+                message: 'New number generated!',
+                type: 'is-info'
+            })
+        }
+    },
+    computed: {
+        randomNumber: {
+            get: function() {
+                return generateLargeRandomNumber()
+            },
+            set: this.get
+        }
+    },
+    data () {
+        return {
+            randomNumber: generateLargeRandomNumber()
+        }
     }
-  }
 }
 </script>
