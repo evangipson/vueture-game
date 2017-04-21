553ad5a2002546c84fc902a190302241a4955aa2
Evan Gipson <evangipson@gmail.com>
2017-04-21 09:49:11 -0500

    Initial commit - firebase read still isn't working
    But works with auth().currentUser?? weird

--- src/components/stats.vue
+++ src/components/stats.vue
@@ -4,15 +4,45 @@
             <div class="heading">
                 <h1 class="title">Statistics Page</h1>
             </div>
-            <p>I'll have some stuff to say about the server.</p>
+            <p v-cloak>Server's Gold: {{serverGold}}</p>
+            <p v-cloak>Total Gold: {{totalGold}}</p>
         </div>
     </section>
 </template>
 
 <script>
+import database from "../js/db"
+
+var serverGold = 0;
+var totalGold = 0;
+
 export default {
+    computed: {
+        serverGold: {
+            get: function() {
+                console.log("Logging started in computed serverGold");
+                var vm = this;
+                database.firebaseInterface.db.ref("/gold/server").on('value', function(snapshot) {
+                    console.log("In the Firebase Promise for /gold/server");
+                    console.log(serverGold);
+                    console.log(snapshot.val());
+                    console.log("Logging ended");
+                    serverGold = snapshot.val();
+                });
+            }
+        },
+        totalGold: {
+            get: function() {
+                database.firebaseInterface.db.ref("/gold/total").on('value', function(snapshot) {
+                    totalGold = snapshot.val();
+                });
+            }
+        }
+    },
     data() {
         return {
+            serverGold: serverGold,
+            totalGold: totalGold
         }
     }
 }
