deb6921580f2bc1076d61b01c2374cd7e7342c56
Evan Gipson <evangipson@gmail.com>
2017-04-21 23:33:21 -0500

    Use Vuefire because I'm done wrestling with native firebase stuff
    Is this defeat?
    
    ...Is anyone listening?

--- src/components/stats.vue
+++ src/components/stats.vue
@@ -4,8 +4,9 @@
             <div class="heading">
                 <h1 class="title">Statistics Page</h1>
             </div>
-            <p v-cloak>Server's Gold: {{serverGold}}</p>
-            <p v-cloak>Total Gold: {{totalGold}}</p>
+            <ul>
+                <li v-for="gold in goldRef">{{ gold['.key'] }}: {{ gold['.value'] }}</li>
+            </ul>
         </div>
     </section>
 </template>
@@ -13,36 +14,13 @@
 <script>
 import database from "../js/db"
 
-var serverGold = 0;
-var totalGold = 0;
-
 export default {
-    computed: {
-        serverGold: {
-            get: function() {
-                console.log("Logging started in computed serverGold");
-                var vm = this;
-                database.firebaseInterface.db.ref("/gold/server").on('value', function(snapshot) {
-                    console.log("In the Firebase Promise for /gold/server");
-                    console.log(serverGold);
-                    console.log(snapshot.val());
-                    console.log("Logging ended");
-                    serverGold = snapshot.val();
-                });
-            }
-        },
-        totalGold: {
-            get: function() {
-                database.firebaseInterface.db.ref("/gold/total").on('value', function(snapshot) {
-                    totalGold = snapshot.val();
-                });
-            }
-        }
+    firebase: {
+        // can bind to either a direct Firebase reference or a query
+        goldRef: database.firebaseInterface.db.ref("/gold")
     },
     data() {
         return {
-            serverGold: serverGold,
-            totalGold: totalGold
         }
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,13 @@ import Vue from "vue"
 import router from "./routes"
 import App from "../components/app.vue"
 import Buefy from "buefy"
+import Vuefire from "vuefire"
 import database from "./db"
 
 // Link buefy so we can use those beufytul components
 Vue.use(Buefy);
+// Link vuefire up as well for crud stuff w/ firebase
+Vue.use(Vuefire);
 // Link up firebase using our config and callbacks
 database.fireInit(fireStatus);
 
@@ -15,6 +18,6 @@ function fireStatus(loggedIn) {
     const app = new Vue({
         el: "#app",
         router,
-        render: h => h(App)
+        render: h => h(App),
     }).$mount('#app');
 }
