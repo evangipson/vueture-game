21313633180e3a6a9f80745fa64d158703c17f12
Evan Gipson <evangipson@gmail.com>
2017-04-30 20:35:58 -0500

    Update user profile w/ purchased businesses
    Yay database interactions!

--- src/components/business.vue
+++ src/components/business.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="column">
         <h2>Owned Businesses</h2>
-        <p>{{userBusinesses}}</p>
+        <p v-if="currentUserBusinessRef">{{this.userBusinesses}}</p>
         <h2>Businesses for sale</h2>
         <h3>Business #1</h3>
-        <p>{{generatedBusiness}}</p>
+        <p>{{this.generatedBusiness}}</p>
         <a class="button is-success" v-on:click="createBusiness()">Create Business</a>
         <a class="button is-info" v-on:click="buyBusiness()">Buy Business</a>
     </div>
@@ -14,12 +14,15 @@
 import business from "../js/business"
 import database from "../js/db"
 
+
+var currentUserBusinessRef = database.firebaseInterface.db.ref("/users/" + database.currentUser().uid + "/businesses");
+
 export default {
     computed: {
         currentUserBusinessRef: {
             get: function() {
                 var vm = this;
-                return currentUserRef.child("businesses").on("value", function(snapshot) {
+                return currentUserBusinessRef.on("value", function(snapshot) {
                     vm.userBusinesses = snapshot.val();
                 });
             }
@@ -30,7 +33,7 @@ export default {
             this.generatedBusiness = business.constructBusiness();
         },
         buyBusiness: function() {
-            currentUserBusinessRef.update(generatedBusiness);
+            currentUserBusinessRef.push().set(this.generatedBusiness);
             // Let the user know we were successful in buying the business
             this.$toast.open({
                 message: 'Business Purchased!',
@@ -40,7 +43,7 @@ export default {
     },
     data() {
         return {
-            userBusinesses: [],
+            userBusinesses: '',
             generatedBusiness: business.constructBusiness()
         }
     }
