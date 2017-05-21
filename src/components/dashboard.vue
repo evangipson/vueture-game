69909dd9ae1b832b077a255638f94c026cb6ad7b
Evan Gipson <evangipson@gmail.com>
2017-05-21 16:41:44 -0500

    Animate our CTAs in the dashboard so users click!

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -14,35 +14,44 @@
             </div>
         </div>
       </div>
-        <!-- card with link to new business -->
-        <!-- maybe use the plus on a smaller scale? -->
-      <div v-if="availableBusinesses" class="column is-half">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title">Interview Somebody!</p>
-          </header>
-          <router-link to="/staff">
-            <div class="card-content plus-center">
-              <svg id="newBusinessPlus" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 42 42" xml:space="preserve">
-                <polygon points="42,20 22,20 22,0 20,0 20,20 0,20 0,22 20,22 20,42 22,42 22,22 42,22"></polygon>
-              </svg>
-            </div>
-          </router-link>
+      <div class="column is-half">
+        <div class="columns">
+          <div style="text-align:center" class="column">
+            <h2><b>Action Center</b></h2>
+          </div>
         </div>
-      </div>
-        <!-- card with link to new staff -->
-      <div v-else class="column is-half">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title">Start a New Business!</p>
-          </header>
-          <router-link to="/new-business">
-            <div class="card-content plus-center">
-              <svg id="newBusinessPlus" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 42 42" xml:space="preserve">
-                <polygon points="42,20 22,20 22,0 20,0 20,20 0,20 0,22 20,22 20,42 22,42 22,22 42,22"></polygon>
-              </svg>
+        <div class="columns">
+          <!-- card with link to new business -->
+          <!-- maybe use the plus on a smaller scale? -->
+          <div v-if="availableBusinesses" class="column is-half">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">Interview Somebody!</p>
+              </header>
+              <router-link to="/staff">
+                <div class="card-content plus-center">
+                  <svg id="newStaffPlus" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 42 42" xml:space="preserve">
+                    <polygon points="42,20 22,20 22,0 20,0 20,20 0,20 0,22 20,22 20,42 22,42 22,22 42,22"></polygon>
+                  </svg>
+                </div>
+              </router-link>
             </div>
-          </router-link>
+          </div>
+          <!-- card with link to new staff -->
+          <div :class="[availableBusinesses ? 'column is-half' : 'column']">
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title">Start a New Business!</p>
+              </header>
+              <router-link to="/new-business">
+                <div class="card-content plus-center">
+                  <svg id="newBusinessPlus" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 42 42" xml:space="preserve">
+                    <polygon points="42,20 22,20 22,0 20,0 20,20 0,20 0,22 20,22 20,42 22,42 22,22 42,22"></polygon>
+                  </svg>
+                </div>
+              </router-link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -51,12 +60,11 @@
 
 <script>
 import Hero from "./hero.vue";
-import OwnedBusinesses from "./owned-businesses.vue";
 import AccountInformation from "./account-information.vue";
 import database from "../js/db";
 
 export default {
-    components: { Hero, OwnedBusinesses, AccountInformation },
+    components: { Hero, AccountInformation },
     mounted: function() {
         var vm = this;
         database.firebaseInterface.db.ref("users/" + database.currentUser().uid + "/businesses").on("value", function(snapshot) {
